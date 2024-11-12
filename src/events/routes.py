8c91f42ea15f22115beb32200596a02694222325
Rahul <rahul.mymail1@gmail.com>
2024-11-12 11:41:14 +0530

    Fix: bug

--- src/events/routes.py
+++ src/events/routes.py
@@ -23,20 +23,20 @@ async def createEvents(event_data: EventcreateModel, session: AsyncSession = Dep
     return new_event
 
 
-@event_router.get('/{event_uid}')
-async def getEventById(event_uid: int, session: AsyncSession = Depends(get_session)) -> dict:
+@event_router.get('/{event_uid}', response_model=Event)
+async def getEventById(event_uid: str, session: AsyncSession = Depends(get_session)) -> dict:
     event = await event_service.get_event(event_uid, session)
 
     if event:
-        return
+        return event
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Event no t found')
 
 
-@event_router.patch('/{event_uid}')
-async def updateEventById(event_uid: int, event_data: EventUpdateModel, session: AsyncSession = Depends(get_session)) -> dict:
+@event_router.patch('/{event_uid}', response_model=Event)
+async def updateEventById(event_uid: str, event_data: EventUpdateModel, session: AsyncSession = Depends(get_session)) -> dict:
     updated_event = await event_service.update_event(event_uid, event_data, session)
-
+    
     if updated_event:
         return updated_event
     else:
@@ -44,7 +44,7 @@ async def updateEventById(event_uid: int, event_data: EventUpdateModel, session:
 
 
 @event_router.delete('/{event_uid}' , status_code=status.HTTP_204_NO_CONTENT)
-async def deleteEventById(event_uid: int, session:AsyncSession = Depends(get_session)):
+async def deleteEventById(event_uid: str, session:AsyncSession = Depends(get_session)):
     delete_event = await event_service.delete_event(event_uid, session)
 
     if delete_event:
--- src/events/service.py
+++ src/events/service.py
@@ -39,28 +39,30 @@ class EventService:
             return None
 
     async def update_event(self, event_uid: str, event_data: EventUpdateModel, session:AsyncSession):
-        event_to_update = self.get_event(event_uid, session)
+        event_to_update = await self.get_event(event_uid, session)
 
         if event_to_update is not None:
             event_update_data_in_dict = event_data.model_dump()
 
+            print(event_update_data_in_dict)
             for k , v in event_update_data_in_dict.items():
                 setattr(event_to_update, k , v)
 
+            event_to_update.updated_at = datetime.now()
             await session.commit()
+            await session.refresh(event_to_update)
             return event_to_update
         else:
             return None
 
     async def delete_event(self, event_uid: str,session: AsyncSession):
         
-        event_to_delete = self.get_event(event_uid, session)
+        event_to_delete = await self.get_event(event_uid, session)
 
         if event_to_delete is not None:
             await session.delete(event_to_delete)
-
             await session.commit()
-            return {}
+            return event_uid
         else:
             return None
 
